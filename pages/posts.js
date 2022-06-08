0008cd212e67e638788f553045a5e6ebacbac3ba
sepiropht <wmbotta@cardiweb.com>
2022-06-08 15:59:10 +0200

    init

---
+++ pages/posts.js
@@ -0,0 +1,32 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+import thumbPortfolio from '../public/images/contents/youtube-how-to-build-portfolio.jpg'
+import thumbHowToUseInkdrop from '../public/images/contents/youtube-how-to-use-inkdrop.jpg'
+import thumbFishWorkflow from '../public/images/contents/youtube-fish-workflow.jpg'
+import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
+
+const Posts = () => (
+  <Layout title="Posts">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Popular Posts
+      </Heading>
+
+      <Section delay={0.1}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="How to build a portfolio website"
+            thumbnail={thumbPortfolio}
+            href="https://www.youtube.com/watch?v=bSMZgXzC9AA"
+          />
+        </SimpleGrid>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Posts
+export { getServerSideProps } from '../components/chakra'
