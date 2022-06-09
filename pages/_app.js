8198397c79f50db8a8c760c02abfc73a17e87b76
sepiropht <wmbotta@cardiweb.com>
2022-06-09 18:17:56 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,21 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
+
+const tagManagerArgs = {
+  gtmId: 'UA-146778312-1'
+}
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
 }
 
 function Website({ Component, pageProps, router }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  })
   return (
     <Chakra cookies={pageProps.cookies}>
       <Fonts />
